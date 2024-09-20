db9d84561cae7a1eb8f48dc986f2bcce480f2fd8
MauriceLaferriere <168125555+MauriceLaferriere@users.noreply.github.com>
2024-09-20 15:23:37 -0400

    STIG script

---
+++ ApplySTIGWIN11.ps1
@@ -0,0 +1,22 @@
+﻿
+try {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force 
+} catch {
+    Write-Error "Failed to install NuGet: $_"
+}
+
+try {
+    if (-not (Get-Module -ListAvailable -Name PowerSTIG)) {
+        Install-Module -Name PowerSTIG -Force
+    }} catch {
+    Write-Error "Failed to install PowerSTIG module: $_"
+}
+try {    (Get-Module PowerStig -ListAvailable).RequiredModules | ForEach-Object {
+        Install-Module -Name $_.Name -RequiredVersion $_.Version -Force
+    }} catch {
+    Write-Error "Failed to install required modules for PowerSTIG: $_"
+}try {    Install-Module SecurityPolicyDsc -Force} catch {
+    Write-Error "Failed to install SecurityPolicyDsc module: $_"
+}try {    Configuration STIGWIN11 {        param        (            [parameter()]            [string]            $NodeName = 'localhost'        )            Import-Module -Name PowerSTIG        Import-DscResource -ModuleName PowerSTIG        Import-DscResource -ModuleName SecurityPolicyDsc -ModuleVersion '2.10.0.0'        Node $NodeName {            WindowsClient BaseLine {                OsVersion   = '11'	            SkipRule    = 'V-253495','V-253480','V-253282'                 Exception   = @{                    'V-253357' = @{                        ValueData = '1' # Required for using Azure Image Builder access to creation                    }                    'V-253491' = @{                        Identity = 'Guests'                     }                }            }        }    }    STIGWIN11} catch {
+    #Write-Error "Failed to compile STIGWIN11 configuration: $_"
+}
