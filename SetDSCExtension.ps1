ab03f27dc0f8e3bcf7f7c24bc1e00a2ad9e69f71
MauriceLaferriere <168125555+MauriceLaferriere@users.noreply.github.com>
2024-09-24 16:20:06 -0400

    Set the DSC extension

---
+++ SetDSCExtension.ps1
@@ -0,0 +1,12 @@
+﻿Param(
+    [parameter(Mandatory)]
+    [string]
+    $vmName
+)
+
+
+Set-AzureVMDscExtension -VM $vmName `
+    -ConfigurationArchive "https://raw.githubusercontent.com/MauriceLaferriere/AVD-stig/main/STIGWIN11.zip" `
+    -ConfigurationName "ApplySTIGWIN11.ps1\STIGWIN11" `
+    -ConfigurationArgument @{ }
+
