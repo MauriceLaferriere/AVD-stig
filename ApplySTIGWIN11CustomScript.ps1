430174b27dc964698e4d0c65378016ed4ac21bfa
MauriceLaferriere <168125555+MauriceLaferriere@users.noreply.github.com>
2024-09-24 08:28:23 -0400

    Force install of modules

--- ApplySTIGWIN11CustomScript.ps1
+++ ApplySTIGWIN11CustomScript.ps1
@@ -5,34 +5,34 @@
 }
 
 try {
-    if (-not (Get-Module -ListAvailable -Name PowerSTIG)) {
-        Install-Module -Name PowerSTIG -Force
-    }} catch {
-    Write-Error "Failed to install PowerSTIG module: $_"
+    Install-Module Az -Force
+} catch {
+    Write-Error "Failed to install Az module: $_"
 }
-try {    (Get-Module PowerStig -ListAvailable).RequiredModules | ForEach-Object {
+
+try {
+    Install-Module Az.DesktopVirtualization -Force
+} catch {
+    Write-Error "Failed to install Az.DesktopVirtualization: $_"
+}
+
+try {
+    Install-Module -Name PowerSTIG -Force
+
+    (Get-Module PowerStig -ListAvailable).RequiredModules | ForEach-Object {
         Install-Module -Name $_.Name -RequiredVersion $_.Version -Force
     }} catch {
     Write-Error "Failed to install required modules for PowerSTIG: $_"
 }try {    Install-Module SecurityPolicyDsc -Force} catch {
     Write-Error "Failed to install SecurityPolicyDsc module: $_"
 }
-try {
-    if (-not (Get-Module -ListAvailable -Name Az.DesktopVirtualization)) {
-        Install-Module Az.DesktopVirtualization -Force
-    }
-} catch {
-    Write-Error "Failed to install Az.DesktopVirtualization: $_"
+try {    Configuration STIGWIN11 {                    Import-Module -Name PowerSTIG        Node 'localhost' {            WindowsClient BaseLine {                OsVersion   = '11'	            SkipRule    = 'V-253495','V-253480','V-253282'                 Exception   = @{                    'V-253357' = @{                        ValueData = '1' # Required for using Azure Image Builder access to creation                    }                    'V-253491' = @{                        Identity = 'Guests'                     }                }            }        }    }    # Compile the configuration    STIGWIN11} catch {
+    Write-Error "Failed to compile STIGWIN11 configuration: $_"
 }
 
 try {
-    if (-not (Get-Module -ListAvailable -Name Az)) {
-        Install-Module Az -Force
-    }
+    # Apply the configuration
+    Start-DSCConfiguration -Path .\STIGWIN11 -Wait -Force -Verbose
 } catch {
-    Write-Error "Failed to install RemoteDesktop: $_"
-}
-try {    Configuration STIGWIN11 {        param        (            [parameter()]            [string]            $NodeName = 'localhost'        )            Import-Module -Name PowerSTIG        Import-DscResource -ModuleName PowerSTIG        Import-DscResource -ModuleName SecurityPolicyDsc -ModuleVersion '2.10.0.0'        Node $NodeName {            WindowsClient BaseLine {                OsVersion   = '11'	            SkipRule    = 'V-253495','V-253480','V-253282'                 Exception   = @{                    'V-253357' = @{                        ValueData = '1' # Required for using Azure Image Builder access to creation                    }                    'V-253491' = @{                        Identity = 'Guests'                     }                }            }        }    }    STIGWIN11    # Apply the configuration
-    Start-DSCConfiguration -Path .\STIGWIN11 -Wait -Force -Verbose} catch {
-    Write-Error "Failed to compile STIGWIN11 configuration: $_"
+    Write-Error "Failed to apply STIGWIN11 configuration: $_"
 }
