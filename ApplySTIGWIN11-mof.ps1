685ec036746da36d88f9ff87b4960a767924919b
MauriceLaferriere <168125555+MauriceLaferriere@users.noreply.github.com>
2024-09-24 14:50:16 -0400

    Compiled configuration

---
+++ ApplySTIGWIN11-mof.ps1
@@ -0,0 +1,15 @@
+﻿try {
+    $folderPath = "C:\Temp"
+    if (-not (Test-Path -Path $folderPath)) {
+        New-Item -Path $folderPath -ItemType Directory
+    }
+
+    # Download the .mof file
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MauriceLaferriere/AVD-stig/main/STIGWIN11.mof" -OutFile "$folderPath\STIGWIN11.mof"
+
+    # Apply the configuration
+    Start-DSCConfiguration -Path $folderPath -Wait -Force -Verbose
+
+} catch {
+    Write-Error "Failed to apply STIGWIN11 configuration: $_"
+}
